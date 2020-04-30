04d4a0af7518a060c1545505997b6bc854b7926c
wraybowling <1120619+wraybowling@users.noreply.github.com>
2020-04-29 22:55:22 -0400

    lots of pin definitions, but still confused about record

--- tri_ger_plus/tri_ger_plus.ino
+++ tri_ger_plus/tri_ger_plus.ino
@@ -16,8 +16,8 @@ ATMEGA328 AU pin configuration & assignment
 pins on chip start at dot and go countner-clockwise
 
 d pins: digital 0-7
-b pins: digital 8-13
-c pins: analog 0-7
+b pins: digital 8-15
+c pins: digital 16-23 analog 0-7
 */
 
 #define top_out        3  //1 (pd3)
@@ -31,7 +31,7 @@ c pins: analog 0-7
 
 #define right_out      5  //9 (pd5)
 #define alt            6  //10 (pd6)
-//#define unknown1     7  //11 (pd7) ??? some sort of reset or enable pin, normally high, triggers low
+#define clock_out      7  //11 (pd7)
 #define led_latch      8  //12 (pb0)
 //#define unknown2     9  //13 (pb1) low for most of clock, then high maybe 20 clocks later, a single pulse low, then full low again 100 clocks later
 //#define unknown3     10 //14 (pb2) a shape like this ---------|----|----|____________|--|__|--
@@ -40,23 +40,37 @@ c pins: analog 0-7
 
 //#define unknown4     13 //17 (pb5 SCK) yet another reset or something similar
 //AVCC                    //18
-#define roll_rate      6  //19 (pc6 adc6) - roll rate
+#define roll_rate      A6 //19 (pc6(22) adc6) - roll rate
 //AREF                    //20
 //GND                     //21
-//#define unknown5     7  //22 (pc7 adc7) - ??? 
-//#define unknown6     14 //23 (pc0 adc0) - ??? a pullup because it's HIGH?
-#define play_pause     15 //24 (pc1 adc1) - play/pause
+//#define unknown5     7  //22 (pc7(23) adc7) - ??? 
+#define retrigger       16 //23 (pc0(16) adc0) - ??? a pullup because it's HIGH?
+#define play_pause     17 //24 (pc1(17) adc1) - play/pause
 
-#define retrigger      16 //25 (pc2)
-#define roll           17 //26 (pc3)
-//? 27 pc4(18) adc4 - ?
-//? 28 pc5(19) adc5 - ?
+#define retrigger2      18 //25 (pc2(18) adc2) - ??? huh?
+#define roll           19 //26 (pc3(19) adc3)
+//#define                 ? 27 (pc4(20) adc4) - ?
+//                          28 (pc5(21) adc5) - ?
 //  29 pc6(20) RESET
 #define top_button     0  //30 (pd0)
 #define left_button    1  //31 (pd1)
 #define right_button   2  //32 (pd2) 
 
+#define tmpl4  {4,0,0,0, 0,0,0,0, 0,0,0,0}
+#define tmpl2  {2,0,0,0, 0,0,0,0, 0,0,0,0}
+#define tmpl0  {0,0,0,0, 0,0,0,0, 0,0,0,0}
+
+int seq_weights[16][12] = {
+   tmpl4, tmpl0, tmpl0, tmpl0,
+   tmpl0, tmpl0, tmpl0, tmpl0,
+   tmpl4, tmpl0, tmpl0, tmpl0,
+   tmpl2, tmpl0, tmpl0, tmpl0
+};
+
 void setup() {
+  pinMode(clock_in, INPUT_PULLUP);
+  pinMode(clock_out, OUTPUT);
+  
   pinMode(top_out, OUTPUT);
   pinMode(left_out, OUTPUT);
   pinMode(right_out, OUTPUT);
@@ -70,14 +84,17 @@ void setup() {
 
 //pinMode(clock_division, INPUT);
 //pinMode(tempo, INPUT);
-  pinMode(roll_rate, INPUT);
   
   pinMode(led_latch, OUTPUT);
   pinMode(led_data, OUTPUT);
   pinMode(led_clock, OUTPUT);
 
+  //unidentified
+  pinMode(20, INPUT);
+
   //init
   digitalWrite(led_latch, HIGH);
+  
 }
 
 byte orng =  B00111110;
@@ -87,9 +104,10 @@ byte white = B00000011;
 byte rgw = green + white;
 
 void loop() {
-
-  delay(100);
-  //if(counter++ > 100) counter = 0;
+  byte d = digitalRead(clock_in);
+  digitalWrite(led_latch, LOW);
+  shiftOut(led_data, led_clock, LSBFIRST, d);
+  digitalWrite(led_latch, HIGH);
 }
 
 
