f99caf37ce1a3cf018564ee214ab526dffa7e93e
wraybowling <1120619+wraybowling@users.noreply.github.com>
2020-05-12 08:58:16 -0400

    identified multiplexing for two knobs, fixed up the LED function, cleaned comments

--- tri_ger_plus/led.h
+++ tri_ger_plus/led.h
@@ -1,9 +1,8 @@
 #ifndef TRIGERPLUS_LED_H_
 #define TRIGERPLUS_LED_H_
-
 #define led_clock  12
 #define led_data   11
 #define led_latch  8
+#endif
 
-void lights();
-
+void lights(byte orange);
--- tri_ger_plus/tri_ger_plus.ino
+++ tri_ger_plus/tri_ger_plus.ino
@@ -33,25 +33,25 @@ c pins: digital 16-23 analog 0-7
 #define alt            6  //10 (pd6)
 #define clock_out      7  //11 (pd7)
 #define led_latch      8  //12 (pb0)
-//#define unknown2     9  //13 (pb1) low for most of clock, then high maybe 20 clocks later, a single pulse low, then full low again 100 clocks later
-//#define unknown3     10 //14 (pb2) a shape like this ---------|----|----|____________|--|__|--
+#define unknown2       9  //13 (pb1) low for most of clock, then high maybe 20 clocks later, a single pulse low, then full low again 100 clocks later
+#define unknown3       10 //14 (pb2) a shape like this ---------|----|----|____________|--|__|--
 #define led_data       11 //15 (pb3 MOSI) two bytes: grgrgrww, orange
 #define led_clock      12 //16 (pb4)
 
-//#define unknown4     13 //17 (pb5 SCK) yet another reset or something similar
+#define unknown4       13 //17 (pb5 SCK) yet another reset or something similar
 //AVCC                    //18
 #define roll_rate      A6 //19 (pc6(22) adc6) - roll rate
 //AREF                    //20
 //GND                     //21
-//#define unknown5     7  //22 (pc7(23) adc7) - ??? 
-#define retrigger       16 //23 (pc0(16) adc0) - ??? a pullup because it's HIGH?
+#define unknown5       A7 //22 (pc7(23) adc7) - ??? 
+#define retrigger      16 //23 (pc0(16) adc0) - ??? a pullup because it's HIGH?
 #define play_pause     17 //24 (pc1(17) adc1) - play/pause
 
-#define retrigger2      18 //25 (pc2(18) adc2) - ??? huh?
+#define retrigger2     18 //25 (pc2(18) adc2) - ??? huh?
 #define roll           19 //26 (pc3(19) adc3)
-//#define                 ? 27 (pc4(20) adc4) - ?
-//                          28 (pc5(21) adc5) - ?
-//  29 pc6(20) RESET
+#define unknown6       20 //27 (pc4(20) adc4) - ?
+#define unknown7       21 //28 (pc5(21) adc5) - ?
+//RESET                   //29 pc6(20)
 #define top_button     0  //30 (pd0)
 #define left_button    1  //31 (pd1)
 #define right_button   2  //32 (pd2) 
@@ -59,6 +59,8 @@ c pins: digital 16-23 analog 0-7
 #define tmpl4  {4,0,0,0, 0,0,0,0, 0,0,0,0}
 #define tmpl2  {2,0,0,0, 0,0,0,0, 0,0,0,0}
 #define tmpl0  {0,0,0,0, 0,0,0,0, 0,0,0,0}
+
+#include "led.h"
 
 int seq_weights[16][12] = {
    tmpl4, tmpl0, tmpl0, tmpl0,
@@ -68,7 +70,7 @@ int seq_weights[16][12] = {
 };
 
 void setup() {
-  pinMode(clock_in, INPUT_PULLUP);
+//  pinMode(clock_in, INPUT_PULLUP);
   pinMode(clock_out, OUTPUT);
   
   pinMode(top_out, OUTPUT);
@@ -94,19 +96,35 @@ void setup() {
 
   //init
   digitalWrite(led_latch, HIGH);
+
+
+  //multiplexer
+  pinMode(unknown2, OUTPUT);
+  pinMode(unknown3, OUTPUT);
+//  pinMode(unknown5, OUTPUT);
+//  pinMode(unknown6, OUTPUT);
+//  pinMode(unknown7, OUTPUT);
+  digitalWrite(unknown2, HIGH);
+  digitalWrite(unknown3, HIGH);
+//  digitalWrite(unknown5, HIGH);
+
+ //unknowns 2,3,4
+  
+  byte clock_division = B000;
+  byte roll_rate      = B010;
+  byte something_else = B100;
+
   
 }
 
-byte orng =  B00111110;
-byte red =   B01010100;
-byte green = B10101000;
-byte white = B00000011;
-byte rgw = green + white;
 
 void loop() {
-  byte d = digitalRead(clock_in);
+
+  byte d = analogRead(A5);
+  
+//  byte d = analogRead(roll_rate);
   digitalWrite(led_latch, LOW);
-  shiftOut(led_data, led_clock, LSBFIRST, d);
+  lights(d);
   digitalWrite(led_latch, HIGH);
 }
 
