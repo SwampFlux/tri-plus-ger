253b825bb0ba30515edb827b541508577387a076
wraybowling <1120619+wraybowling@users.noreply.github.com>
2020-05-28 08:53:11 -0400

    cleaned up comments for mux section, #4 hunting for the missing tempo CV

--- tri_ger_plus/tri_ger_plus.ino
+++ tri_ger_plus/tri_ger_plus.ino
@@ -33,42 +33,47 @@ c pins: digital 16-23 analog 0-7
 #define alt            6  //10 (pd6)
 #define clock_out      7  //11 (pd7)
 #define led_latch      8  //12 (pb0)
-#define mux_select_0   9  //13 (pb1) low for most of clock, then high maybe 20 clocks later, a single pulse low, then full low again 100 clocks later
-#define mux_select_1   10 //14 (pb2) a shape like this ---------|----|----|____________|--|__|--
-#define led_data       11 //15 (pb3 MOSI) two bytes: grgrgrww, orange
+#define mux_select_0   9  //13 (pb1)
+#define mux_select_1   10 //14 (pb2)
+#define led_data       11 //15 (pb3 MOSI) - 2 bytes: grgrgrww, orange
 #define led_clock      12 //16 (pb4)
 
-#define mux_select_2   13 //17 (pb5 SCK) yet another reset or something similar
-//AVCC              tem      //18
+#define mux_select_2   13 //17 (pb5 SCK)
+//AVCC                    //18
 #define mux_voltage    A6 //19 (pc6(22) adc6) - read from lots of knobs
 //AREF                    //20
 //GND                     //21
-#define unknown7       23 //22 (pc7(23) adc7) - ??? 
+#define findme_1       23 //22 (pc7(23) adc7) - likely unused
 #define retrigger      A0 //23 (pc0(16) adc0)
 #define play_pause     A1 //24 (pc1(17) adc1)
 
-#define unknown2       18 //25 (pc2(18) adc2) - ??? reads high. a reference?
+#define master_trig    18 //25 (pc2(18) adc2) - retrigger(1) play(2) from daisy chain
 #define roll           19 //26 (pc3(19) adc3)
-#define unknown4       20 //27 (pc4(20) adc4) - ??? read roughly half voltage one time. otherwise matches unknown2 output
-#define unknown5       21 //28 (pc5(21) adc5) - ??? always low. nothing?
+#define findme_3       20 //27 (pc4(20) adc4) - ??? read roughly half voltage one time. otherwise matches unknown2 output
+#define findme_4       21 //28 (pc5(21) adc5) - ??? always low. nothing?
 //RESET                   //29 pc6(20)
 #define top_button     0  //30 (pd0)
 #define left_button    1  //31 (pd1)
 #define right_button   2  //32 (pd2)
 
-
-// multiplexed analog signals
+/* multiplexed analog signals
+ * all are read on A6 via the getMux() function
+ */
 #define clock_div_knob       B000 // 0
 #define clock_div_cv         B001 // 1
-#define tempo_knob           B010 // 2
+#define tempo_knob           B010 // 2 - from master via ribbon cable if jumper configured
 #define roll_rate_knob       B011 // 3
 #define roll_rate_cv         B100 // 4
-#define host_vs_slave        B101 // 5 (high vs half voltage)
+#define host_vs_slave        B101 // 5 - high vs half voltage, respectively
 #define record_button        B110 // 6
 #define record_button_master B111 // 7
 
+/*
+ * TODO section
+ * these things have not been found yet
+ */
 
-//#include "led.h"
+#define tempo_cv             UNKNOWN
 
 
 
@@ -92,26 +97,32 @@ void setup() {
   pinMode(led_latch, OUTPUT);
   pinMode(led_data, OUTPUT);
   pinMode(led_clock, OUTPUT);
+
+  pinMode(findme_4, INPUT);
 }
 
+
+bool isHigh = false;
+bool yepnope = false;
+
 void loop() {
+//  yepnope = !yepnope;
+//  digitalWrite(test_switch, yepnope);
+//  byte test_signal = byte( 1 << (analogRead(unknown4) / 128) );
+//  lights(0, test_signal);
+//  delay(500);
 
-
-//  digitalWrite(unknown5, HIGH);
-//  delay(10);
-//  digitalWrite(unknown5, LOW);
-//  delay(20);
-
-//  int _roll_rate_cv = ;
-
-  byte test_signal = byte( 1 << (getMux(roll_rate_cv) / 128) );
-//  byte test_signal = byte( 1 << digitalRead(unknown7) );
+  bool gate = digitalRead(findme_4);
+  if(gate){
+    if(!isHigh){
+      yepnope = !yepnope;
+    }
+    isHigh = true;
+  }else{
+    isHigh = false;
+  }
   
-  lights(0, test_signal);
-//  counter << 1;
-//  if(counter >= 1000) counter = 1;
-  
-//  delay(100);
+  lights(byte(1 << (4 * yepnope)), byte(1 << gate));
 }
 
 int getMux(char channels) {
